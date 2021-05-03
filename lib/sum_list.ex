9e37550abc8c140ba475b215561bd7e50ba0146f
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-03 16:55:47 -0300

    :recycle: changes the function to one line

--- lib/sum_list.ex
+++ lib/sum_list.ex
@@ -26,9 +26,7 @@ defmodule SumList do
   """
   def call(list), do: sum(list, 0)
 
-  defp sum([], acc) do
-    acc
-  end
+  defp sum([], acc), do: acc
 
   defp sum([head | tail], acc) do
     acc = acc + head
